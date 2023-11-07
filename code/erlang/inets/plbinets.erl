0485ad907d16ba925c67f8277e285befc1d073da
Christopher Augustus <repos@c4augustus.com>
2023-11-07 20:54:16 +0000

    enhance plbinets into an acquirable singleton process instead of just intended for escript

--- code/erlang/inets/plbinets.erl
+++ code/erlang/inets/plbinets.erl
@@ -1,14 +1,24 @@
--export([main/1]).
+-module(plbinets).
+-export([acquire/0, init/0]).
+-define(PROCESS, list_to_atom(atom_to_list(?MODULE) ++ "_single")).
 
-main(_) ->
-  ok = inets:start(),
-  {ok, _} = inets:start(httpd, [
-    {server_name,   "plbinets"  },
+acquire() ->
+  case whereis(?PROCESS) of
+    Pid when is_pid(Pid) -> Pid;
+    _ -> spawn(?MODULE, init, [])
+  end.
+
+init() ->
+  ok = application:ensure_started(inets),
+  {ok,PidHttpd} = inets:start(httpd, [
+    {server_name,   "plbweb"    },
     {server_root,   "."         },
     {document_root, "."         },
     {bind_address,  "localhost" },
     {port,          8088        }
   ]),
+  register(?PROCESS, self()),
   receive
-    stop -> ok
+    stop -> inets:stop(httpd, PidHttpd), ok;
+    stop_inets -> inets:stop(), ok
   end.
