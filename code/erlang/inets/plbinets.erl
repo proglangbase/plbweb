ea21214c33ffa4ef7a49e6bf1ed676da76b78363
Christopher Augustus <repos@c4augustus.com>
2023-11-08 18:52:30 +0000

    improve plbinets

--- code/erlang/inets/plbinets.erl
+++ code/erlang/inets/plbinets.erl
@@ -1,3 +1,10 @@
+%%
+%%  Copyright © 2023 Christopher Augustus
+%%
+%%  This Source Code Form is subject to the terms of the Mozilla Public
+%%  License, v. 2.0. If a copy of the MPL was not distributed with this
+%%  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+%%
 -module(plbinets).
 -export([acquire/0, init/0]).
 -define(PROCESS, list_to_atom(atom_to_list(?MODULE) ++ "_single")).
@@ -10,12 +17,12 @@ acquire() ->
 
 init() ->
   ok = application:ensure_started(inets),
-  {ok,PidHttpd} = inets:start(httpd, [
-    {server_name,   "plbweb"    },
-    {server_root,   "."         },
-    {document_root, "."         },
-    {bind_address,  "localhost" },
-    {port,          8088        }
+  {ok, PidHttpd} = inets:start(httpd, [
+    {server_name,   "proglangbase.org" },
+    {server_root,   "."                },
+    {document_root, "."                },
+    {bind_address,  "localhost"        },
+    {port,          8088               }
   ]),
   register(?PROCESS, self()),
   receive
