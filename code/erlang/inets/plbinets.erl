5b07c04a8c914520ca09450ff5e150bf67a187a3
Christopher Augustus <repos@c4augustus.com>
2024-01-12 19:19:30 +0000

    replace index.html and its retrieval via builtins mod_alias and mod_get with our own module request handling via do/1

--- code/erlang/inets/plbinets.erl
+++ code/erlang/inets/plbinets.erl
@@ -6,7 +6,7 @@
 %%  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %%
 -module(plbinets).
--export([acquire/0, init/0]).
+-export([acquire/0, do/1, init/0]).
 -define(NAME_SINGLETON, list_to_atom(atom_to_list(?MODULE) ++ "_single")).
 
 acquire() ->
@@ -23,11 +23,25 @@ init() ->
         {document_root,   "."                   },
         {bind_address,    "localhost"           },
         {port,            8088                  },
-        {modules,         [mod_alias, mod_get]  },
-        {directory_index, ["index.html"]        }
+        {modules,         [plbinets]            }
     ]),
     register(?NAME_SINGLETON, self()),
     receive
         stop        -> inets:stop(httpd, PidHttpd), ok;
         stop_inets  -> inets:stop(), ok
     end.
+
+do(_) -> %% ModData) -> {proceed, OldData} | {proceed, NewData} | {break, NewData} | done
+    {break, [{response, {200,
+        "<!DOCtYPE html>"
+        "<html>"
+        "<head>"
+        "    <title>proglangbase</title>"
+        "</head>"
+        "<body>"
+        "    <h1>proglangbase</h1>"
+        "    <p>Erlang</p>"
+        "</body>"
+        "</html>"
+        }}]}.
+
