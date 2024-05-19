b2150fe15725027630615ad5438d186430918fa2
Christopher Augustus <repos@c4augustus.com>
2024-05-19 18:55:14 +0100

    move HTML content to a new separate module plbdb

---
+++ code/erlang/db/plbdb.erl
@@ -0,0 +1,31 @@
+%%
+%%  Copyright © 2024 Christopher Augustus
+%%
+%%  This Source Code Form is subject to the terms of the Mozilla Public
+%%  License, v. 2.0. If a copy of the MPL was not distributed with this
+%%  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+%%
+-module(plbdb).
+-export([home/0]).
+
+home() ->
+  "<!DOCtYPE html>"
+  "<html>"
+  "<head>"
+  "  <title>proglangbase</title>"
+  "</head>"
+  "<body>"
+  "  <h1>proglangbase</h1>"
+  "  <table>"
+  "    <tr><td>lang   </td><td>para </td></tr>"
+  "    <tr><td>APL    </td><td>AP   </td></tr>"
+  "    <tr><td>BQN    </td><td>AP   </td></tr>"
+  "    <tr><td>C      </td><td>AP   </td></tr>"
+  "    <tr><td>Erlang </td><td>AP   </td></tr>"
+  "    <tr><td>F#     </td><td>FP   </td></tr>"
+  "    <tr><td>J      </td><td>AP   </td></tr>"
+  "    <tr><td>PHP    </td><td>PP   </td></tr>"
+  "  </table>"
+  "</body>"
+  "</html>"
+  .
--- code/erlang/inets/plbinets.erl
+++ code/erlang/inets/plbinets.erl
@@ -32,25 +32,4 @@ init() ->
   end.
 
 do(_) -> %% ModData) -> {proceed, OldData} | {proceed, NewData} | {break, NewData} | done
-  {break, [{response, {200,
-    "<!DOCtYPE html>"
-    "<html>"
-    "<head>"
-    "  <title>proglangbase</title>"
-    "</head>"
-    "<body>"
-    "  <h1>proglangbase</h1>"
-    "  <table>"
-    "    <tr><td>lang   </td><td>para </td></tr>"
-    "    <tr><td>APL    </td><td>AP   </td></tr>"
-    "    <tr><td>BQN    </td><td>AP   </td></tr>"
-    "    <tr><td>C      </td><td>AP   </td></tr>"
-    "    <tr><td>Erlang </td><td>AP   </td></tr>"
-    "    <tr><td>F#     </td><td>FP   </td></tr>"
-    "    <tr><td>J      </td><td>AP   </td></tr>"
-    "    <tr><td>PHP    </td><td>PP   </td></tr>"
-    "  </table>"
-    "</body>"
-    "</html>"
-    }}]}.
-
+  {break, [{response, {200, plbdb:home()}}]}.
