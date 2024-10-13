94000c81525ecb4723aeb3036410849d8ba002c4
Christopher Augustus <repos@c4augustus.com>
2024-10-13 16:58:14 +0100

    rename plbweb.erl to plbwebinets.erl

--- code/erlang/plbwebinets.erl
+++ code/erlang/plbwebinets.erl
@@ -5,7 +5,7 @@
 %%  License, v. 2.0. If a copy of the MPL was not distributed with this
 %%  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %%
--module(plbweb).
+-module(plbwebinets).
 -export([acquire/0, monitor/1, start/2]).
 -include("../../dep/plbcom/code/erlang/config.hrl").
 
